0f18fc48319eadb0cbf23d5cea7da00d5b7e6c75
HR-SU <1286804583@qq.com>
2020-07-17 14:45:25 +0800

    fix: fix unit test bug in RecommendControllerTest

--- backend/src/test/java/com/prinpedia/backend/controller/RecommendControllerTest.java
+++ backend/src/test/java/com/prinpedia/backend/controller/RecommendControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,6 +22,7 @@ class RecommendControllerTest {
 
     @DisplayName("Rank controller")
     @Test
+    @WithMockUser(username = "test")
     public void rankTest() throws Exception {
         MvcResult result = mockMvc
                 .perform(MockMvcRequestBuilders.get("/recommend")
