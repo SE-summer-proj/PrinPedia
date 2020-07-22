91a775af1c2f157fb5441e20c29d588cb65f4d71
HR-SU <1286804583@qq.com>
2020-07-22 19:25:15 +0800

    feat: implement algorithm to extract contents from wiki markup

--- backend/src/test/java/com/prinpedia/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/prinpedia/backend/BackendApplicationTests.java
@@ -1,6 +1,5 @@
 package com.prinpedia.backend;
 
-import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
