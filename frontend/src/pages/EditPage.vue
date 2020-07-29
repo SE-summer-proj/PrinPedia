accae3c5c959c0ab760401019c98b22242be7953
wutianlong <lycheenut@163.com>
2020-07-29 11:23:04 +0800

    Added button to submit edition.

--- frontend/src/pages/EditPage.vue
+++ frontend/src/pages/EditPage.vue
@@ -4,19 +4,20 @@
     <el-main>
       <el-tabs v-model="activeName">
         <el-tab-pane label="预览" name="preview">
-          <VueWikitext :source="wikiData" />
+          <VueWikitext :source="wikiData.wikiText" />
         </el-tab-pane>
         <el-tab-pane label="编辑" name="editing">
-          <el-input type="textarea" v-model="wikiData" :rows="15" />
+          <el-input type="textarea" v-model="wikiData.wikiText" :rows="15" />
         </el-tab-pane>
       </el-tabs>
+      <el-button @click="submit">提交修改</el-button>
     </el-main>
   </el-container>
 </template>
 
 <script>
     import Header from "@/components/Header";
-    import {GET} from "@/ajax";
+    import {GET, POST} from "@/ajax";
     import {Constants} from "@/utils/constants";
     import VueWikitext from "@/components/VueWikitext";
     export default {
@@ -35,6 +36,14 @@
                 }, (data) => {
                     this.wikiData = data.extraData;
                 })
+            },
+            submit() {
+                return POST(Constants.editUrl, {
+                    title: this.wikiData.title,
+                    wikiText: this.wikiData.wikiText
+                }, (data) => {
+                    this.$message.info(data.message);
+                });
             }
         },
         mounted() {
