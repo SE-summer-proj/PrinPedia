d303d19c6dbc7b6c5de39027998945b682f917fe
HR-SU <1286804583@qq.com>
2020-07-30 15:29:02 +0800

    fix: fix some bugs

--- backend/src/test/java/com/prinpedia/backend/dataProcess/InitEntryWikiText.java
+++ backend/src/test/java/com/prinpedia/backend/dataProcess/InitEntryWikiText.java
@@ -2,21 +2,40 @@ package com.prinpedia.backend.dataProcess;
 
 import com.prinpedia.backend.dao.EntryDao;
 import com.prinpedia.backend.entity.Entry;
+import com.prinpedia.backend.repository.ElasticEntryRepository;
+import com.prinpedia.backend.repository.EntryNodeRepository;
+import com.prinpedia.backend.repository.EntryRepository;
+import com.prinpedia.backend.repository.UserRepository;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.io.*;
 
+@ActiveProfiles(profiles = {"test"})
 @SpringBootTest
 public class InitEntryWikiText {
     @Autowired
     private EntryDao entryDao;
 
+    @Autowired
+    private EntryRepository entryRepository;
+
+    @Autowired
+    private ElasticEntryRepository elasticEntryRepository;
+
+    @Autowired
+    private EntryNodeRepository entryNodeRepository;
+
     @Test
     @Disabled
     public void entryWikiText() throws IOException {
+        entryRepository.deleteAll();
+        elasticEntryRepository.deleteAll();
+        entryNodeRepository.deleteAll();
+
         String path = "G:\\webWorkspace\\prinpedia\\selected_simplified.txt";
 
         File file = new File(path);
