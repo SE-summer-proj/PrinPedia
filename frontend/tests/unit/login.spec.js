a3b2898c8bd41e86bbca6fe27c9cc5fe05717c31
zzy987 <67889264+zzy987@users.noreply.github.com>
2020-07-31 15:24:49 +0800

    Add files via upload
    只解决了登录的测试。注册没解决。同一套方法行不通。

--- frontend/tests/unit/login.spec.js
+++ frontend/tests/unit/login.spec.js
@@ -1,35 +1,55 @@
 import { shallowMount, createLocalVue, mount } from "@vue/test-utils";
 import dialog from "@/components/LoginDialog.vue"
 import ElementUI from "element-ui";
+import Vuex from "vuex"
 
 const localVue = createLocalVue();
 localVue.use(ElementUI);
+localVue.use(Vuex);
 
 jest.mock("axios", () => ({
-    post: () => Promise.resolve({ status: 0 })
+    post: () => Promise.resolve({
+        message: "succeed.",
+        status: 0
+    })
 }));
 
 describe("init", () => {
     it("init state", () => {
         const wrapper = shallowMount(dialog, {
-            localVue
+            localVue,
+            // mocks: {
+            //     axios
+            // },
         });
         /**
          * 检查data的初始状态
          */
         expect(wrapper.vm.loginForm.username).toEqual('');
         expect(wrapper.vm.loginForm.password).toEqual('');
+        expect(wrapper.vm.registerForm.username).toEqual('');
+        expect(wrapper.vm.registerForm.password).toEqual('');
     });
 });
+
+const $store = {
+    commit: () => "true"
+};
+const $router = {
+    push: () => "true"
+};
 
 /**
  * 异步和mock
  */
-describe("login (async function)", () => {
+describe("login", () => {
 
     it("login success by call function", () => {
-        const wrapper = shallowMount(dialog, {
-            localVue
+        const wrapper = mount(dialog, {
+            localVue,
+            // mocks: {
+            //     axios
+            // },
         });
         /**
          * 直接调用组件的method，相当于this.login()
@@ -43,6 +63,10 @@ describe("login (async function)", () => {
          */
         const wrapper = mount(dialog, {
             localVue,
+            mocks: {
+                $store,
+                $router
+            },
         });
         /**
          * 通过css选择器找到登录按钮，并检查是否找到
@@ -53,6 +77,53 @@ describe("login (async function)", () => {
          * 模拟点击。由于login是async的，因此需要await点击完成
          */
         await btn.trigger('click');
-        expect(wrapper.vm).toBeTruthy();
+        wrapper.vm.$nextTick(() => {
+            expect(wrapper.vm.isLogged).toBeTruthy();
+        });
+    });
+});
+
+describe("register", () => {
+
+    it("register success by call function", () => {
+        const wrapper = mount(dialog, {
+            localVue,
+            // mocks: {
+            //     axios
+            // },
+        });
+        /**
+         * 直接调用组件的method，相当于this.login()
+         */
+        wrapper.vm.activeName = "register";
+        expect(wrapper.vm.register()).resolves.toBeTruthy();
+        // expect(wrapper.vm.activeName).toEqual("login");
+    });
+
+    it("register success by click", async () => {
+        /**
+         * 如果使用element-ui等第三方组件库，不要使用shallowMount
+         */
+        const wrapper = mount(dialog, {
+            localVue,
+            // mocks: {
+            //     axios
+            // },
+        });
+        /**
+         * 通过css选择器找到登录按钮，并检查是否找到
+         */
+        wrapper.vm.activeName = "register";
+        // await wrapper.vm.$nextTick();
+        const btn = wrapper.find('#regi-btn');
+        expect(btn.exists()).toBe(true);
+        /**
+         * 模拟点击。由于login是async的，因此需要await点击完成
+         */
+        await btn.trigger('click');
+        console.log(wrapper.vm.activeName);
+        wrapper.vm.$nextTick(() => {
+            expect(wrapper.vm.activeName).toEqual("login");
+        });
     });
 });
