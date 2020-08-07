790d1ad775759138d1b70ee0ee8a6948a3bcac34
HR-SU <1286804583@qq.com>
2020-08-07 16:19:42 +0800

    feat: implement statics repository

---
+++ backend/src/main/java/com/prinpedia/backend/repository/SearchStaticsRepository.java
@@ -0,0 +1,13 @@
+package com.prinpedia.backend.repository;
+
+import com.prinpedia.backend.entity.SearchStatics;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public interface SearchStaticsRepository extends JpaRepository<SearchStatics, Integer> {
+    SearchStatics findByDate(Date date);
+
+    List<SearchStatics> findAllByDateBetween(Date start, Date end);
+}
