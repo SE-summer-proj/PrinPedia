0a60aa6eb858e70b9aaabfa695367866ae3d657f
HR-SU <1286804583@qq.com>
2020-09-05 20:06:33 +0800

    fix: update data process function

--- backend/src/test/java/com/prinpedia/backend/dataProcess/InitElasticJson.java
+++ backend/src/test/java/com/prinpedia/backend/dataProcess/InitElasticJson.java
@@ -175,4 +175,58 @@ public class InitElasticJson {
             num++;
         }
     }
+
+    @Test
+    public void InitMySQLEntryInfoCsvFile() throws IOException {
+        String path = "G:\\webWorkspace\\prinpedia\\page_summary\\";
+        int num = 1;
+
+        while(num <= 1) {
+            String src = path + "Page_00" + num + "_summary.txt";
+            String dest = path + "Page_00" + num + "_entry_info_csv.csv";
+
+            File srcFile = new File(src);
+            FileReader fileReader = new FileReader(srcFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            File destFile = new File(dest);
+            if (!destFile.exists()) {
+                if (!destFile.createNewFile()) {
+                    System.out.println("Create file error");
+                    return;
+                }
+                destFile = destFile.getAbsoluteFile();
+            }
+            FileOutputStream fos = new FileOutputStream(destFile);
+            OutputStreamWriter osw =
+                    new OutputStreamWriter(fos, StandardCharsets.UTF_8);
+            BufferedWriter bw = new BufferedWriter(osw);
+            System.out.println("Opening file " + src +
+                    " succeeded, starting writing");
+
+            String tmp, content;
+            int count = 0;
+            while ((tmp = bufferedReader.readLine()) != null) {
+                content = "";
+                String[] strings = tmp.split("\\t");
+                if (strings.length > 1) {
+                    strings[1] = strings[1]
+                            .replace("\"", "\"\"");
+                    content = strings[0] + ",\"" + strings[1] + "\"\n";
+                }
+                bw.write(content);
+                count++;
+            }
+
+            bw.close();
+            osw.close();
+            fos.close();
+            bufferedReader.close();
+            fileReader.close();
+            System.out.println("Writing to file " + dest +
+                    " finished, count: " + count);
+
+            num++;
+        }
+    }
 }
